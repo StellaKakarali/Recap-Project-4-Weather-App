297b1fd4724ba93e14d02ef0ebc9026634fd2c69
Nadine Grantz <nadine.grantz@googlemail.com>
2024-01-12 16:44:25 +0100

    add solution without handleAddActivity

--- src/ActivityForm.js
+++ src/ActivityForm.js
@@ -1,4 +1,40 @@
-import React from "react";
+import { useState } from "react";
 import "./ActivityForm.css";
 
-function ActivityForm {}
+export default function ActivityForm({ onAddActivity }) {
+  //console.log(onAddActivity);
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const newActivity = {
+      name: event.target.elements.activity.value,
+      isForGoodWeather: event.target.elements.weatherActivity.checked,
+    };
+
+    onAddActivity(newActivity);
+    console.log("ActivityForm: ", newActivity);
+
+    event.target.reset();
+    event.target.elements.activity.focus();
+  }
+
+  return (
+    <div>
+      <h1>Add new Activity</h1>
+      <form onSubmit={handleSubmit}>
+        <label> Activity Name</label>
+        <input
+          id="activity"
+          type="text"
+          name="activity"
+          placeholder="Walking"
+        />
+        <label>Good weather</label>
+        <input id="weather" type="checkbox" name="weatherActivity" />
+        <button id="button" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
