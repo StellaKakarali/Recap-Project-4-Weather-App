50039fa4ff34bb4e13cad3a91648d3bf684d5199
Nadine Grantz <nadine.grantz@googlemail.com>
2024-01-14 23:21:39 +0100

    add folders/files to order

---
+++ src/ActivityForm/ActivityForm.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import "./ActivityForm.css";
+
+export default function ActivityForm({ onAddActivity }) {
+  //console.log(onAddActivity);
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const newActivity = {
+      name: event.target.elements.activity.value,
+      isForGoodWeather: event.target.elements.weatherActivity.checked,
+    };
+
+    onAddActivity(newActivity);
+    console.log("ActivityForm: ", newActivity);
+
+    event.target.reset();
+    event.target.elements.activity.focus();
+  }
+
+  return (
+    <div>
+      <h1>Add new Activity</h1>
+      <form onSubmit={handleSubmit}>
+        <label> Add New Activity: </label>
+        <section className="beside">
+          <label>Name: </label>
+          <input
+            id="activity"
+            type="text"
+            name="activity"
+            placeholder="Walking"
+          />
+        </section>
+        <section className="beside">
+          <label>Good-weather: </label>
+          <input id="weather" type="checkbox" name="weatherActivity" />
+        </section>
+        <button id="button" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
